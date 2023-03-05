08a8a5d31b0e901b639c7ec3842f7d0c445d7d76
kleon6436 <knleon.801.jpt@outlook.jp>
2023-03-05 09:52:21 +0900

    コマンドライン引数を設定できるように変更。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Input city name for which you want to check the weather.
+    #[arg(short, long)]
+    city_name: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", &args.city_name);
 }
